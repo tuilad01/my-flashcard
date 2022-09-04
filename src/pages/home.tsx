b940194ebf634c9fbd39dbdc39e04350afd23258
dat truong <thelegendaglx@gmail.com>
2022-09-04 17:08:49 +0700

    ok 1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
 import DataTable from "../components/data-table/data-table";
 import Data from "../mockData";
 import { useNavigate } from "react-router-dom";
+
+import { Button, Form } from "react-bootstrap";
 
 import manageIndexedDb from "../datastore/manageIndexedDb";
 import GroupSchema from "../datastore/schemas/group-schema";
@@ -9,32 +12,63 @@ import Group from "../interfaces/IGroup";
 function HomePage() {
     let navigate = useNavigate()
 
-    manageIndexedDb.add(GroupSchema.storeName, new Group("group 1"))
-
-    const onAdd = () => {
-        navigate("/group/add")
-    }
-
-    const onEdit = (item: any) => {
-        if (item && item.id) {
-            navigate(`/group/${item.id}`)
-        }
-    }
-
-    const onDelete = (item: any) => {
-
-    }
-
-    const onDeleteAll = (items: any[]) => {
-
-    }
+    const [form, setForm] = useState<{name: string, description: string}>({name: "", description: ""});
+    
 
     
 
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // if (form.name) {
+        //     const group = new Group(form.name, form.description)
+        //     manageIndexedDb.add(GroupSchema.storeName, group);
+        // }
+        
+        console.log("submit")
+    }
+
+    const onChangeForm = (event: any, field: string) => {
+        const value = event.currentTarget.value
+        setForm(pre => ({...pre, [field]: value}));
+    }
+
+    // const onAdd = () => {
+    //     navigate("/group/add")
+    // }
+
+    // const onEdit = (item: any) => {
+    //     if (item && item.id) {
+    //         navigate(`/group/${item.id}`)
+    //     }
+    // }
+
+    // const onDelete = (item: any) => {
+
+    // }
+
+    // const onDeleteAll = (items: any[]) => {
+
+    // }
+
+
+
     return (
-        <DataTable
-            dataSource={Data.tableData}
-            columns={Data.columns} />
+        <div>
+            <Form onSubmit={e => onSubmit(e)}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" value={form.name} onChange={e => onChangeForm(e, "name")} placeholder="Name" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control type="text" value={form.description} onChange={e => onChangeForm(e, "description")} placeholder="Description" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
     );
 }
 
